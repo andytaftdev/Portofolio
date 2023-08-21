e6200fb08f1e8e13862e05e9b3b634239c9f68e1
andytaftdev <andytaft2021@gmail.com>
2023-08-21 11:33:06 +0700

    commit-1

---
+++ dist/js/script.js
@@ -0,0 +1,31 @@
+const scroll = document.querySelector(".navbar");
+const navheight = scroll.offsetHeight;
+const animate = document.querySelector(".animate")
+const balls = document.querySelector(".balls")
+const shadow = document.querySelector(".shadow");
+const body = document.querySelector("body");
+const slide1 = document.querySelector(".slide1");
+
+document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navheight + "px"
+);
+
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        balls.classList.toggle("load");
+        shadow.classList.toggle("gone");
+       }, 5900);
+       setTimeout(() => {
+        slide1.classList.toggle("show");
+       }, 6200);
+
+
+       setTimeout(() => {
+        balls.classList.toggle("gone");
+        animate.classList.toggle("gone");
+        body.classList.toggle("enable");
+        
+        
+       }, 6400);
+});
