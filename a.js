4af580f34f33c46fa425077bbee813bc51d6b9f4
andytaftdev <andytaft2021@gmail.com>
2023-09-11 13:48:42 +0700

    commit-14

--- a.js
+++ a.js
@@ -1,35 +1,197 @@
+// var vallian = Math.random() * 30;
+// var vallian = Math.floor(vallian);
 
-x = 1;
-y = 3;
+// if (vallian > 17) {
+//   console.log("Vallian sudah cukup umur");
+//   console.log(vallian);
+// } else if (vallian < 17) {
+//   console.log("vallian belum cukup umur");
+//   console.log(vallian);
+// }
+
+// var hari = prompt('masukan nama hari!');
+// var hari = hari.toLowerCase();
+
+// if(hari == 'senin'){
+//   alert('diniyyah-diniyyah-IT-Rest-IT-IT-Rest-English-English');
+// } 
+// else if (hari == 'selasa'){
+//   alert('Diniyyah-diniyyah-IT-Rest-english-english-rest-IT-IT');
+
+// }
+// else if (hari == 'rabu'){
+//   alert('diniyah-diniyah-IT-Rest-English-English-Rest-IT-IT');
+
+// }
+// else if (hari == 'kamis'){
+//   alert('IT-IT-IT-Rest-diniyah-diniyah-rest-english-english');
+
+// }
+// else if (hari == 'jumat'){
+//   alert('english-diniyah-IT-Rest-Homeroom-Homeroom-Rest-IT-IT');
+
+// }
+// else{
+//   alert('tidak ada hari itu');
+
+// }
+
+// var number = prompt('masukkan angka')
+// var number = number % 2 
+
+// if(number == 0){
+//   alert('angka itu genap')
+// } else if(number == 1) {
+//   alert('angka itu ganjil')
+
+// }
+
+
+// switch
+
+// var hari = prompt('masukkan hari!!');
+// var mapel;
+
+// switch (hari) {
+//   case 'senin':
+//     alert('hari ini belajar')
+
+//     break;
+//     case 'selasa':
+//       alert('hari ini belajardsd')
+
+//       break;
+
+//   default:
+//     alert('salahh')
+
+
+// }
+
+
+// var nilai = prompt('masukkan nilai');
+
+
+// switch (true) {
+//   case nilai < 60:
+//     alert('nilai anda D (kurang)');
+//     break;
+//     case nilai > 60 && nilai <= 75:
+//     alert('nilai anda C (Cukup)');
+//     break;
+//     case nilai > 75 && nilai <= 85:
+//     alert('nilai anda B (Baik)');
+//     break;
+//     case nilai > 85 && nilai <= 100:
+//     alert('nilai anda A (Ahsan)');
+//     break;
+
+//   default: 
+//   alert('nilai yang anda masukkan salah');
+// }
+
+
+//ternary
+// var nama = prompt('nama')
+// var jawaban = (nama.toLowerCase() == 'kak fahmi') ? 'benar': 'salah';
+// alert(`jawaban yang anda kirim : ${jawaban}`);
+
+
+// nested branches
+
+// var username = prompt('masukin user : ')
+// var pass = prompt('masukkan password : ')
+
+// if (username == 'fahmi17') {
+//   if (pass == '123') {
+//     alert('anda berhasil login')
+//   } else {
+//     alert('password anda salah')
+//   }
+
+// } else {
+//   alert('tidak ada username')
+// }
 
 
 
+var jawaban1 = prompt('apakah kak fahmi baik? A : Iya B : Salah C : Gatau')
 
 
-const btn = document.getElementById('btn-count');
-var box1h= document.getElementById('re-1');
-var box1w = box1h.offsetWidth;
-var box2h= document.getElementById('re-2');
-var box2w = box2h.offsetWidth;
+switch (true) {
+  case jawaban1 == 'A':
+    alert('Betul ka fahmi orang baik')
+    break;
 
-
-
-
-
-btn.addEventListener("click", () => {
-
+  default:
+    if (jawaban1 == 'B'){
+  alert('Salah, ka fahmi adalah orang baik');
   
-  var hasil = kali(box1w,box2w);
-  console.log(hasil);
-  });
-function kali(a,b,){
-     var total1;
-     var total2;
-     var total;
-     total1 = a * a;
-     total2 = b * b;
-     total = total1 + total2;
-     return total
 
-     
+  }
+
+
+   else {
+    if (jawaban1 == 'C'){
+      alert('harus tau kalo ka fahmi adalah orang baik');
+    
+      } else{
+        alert(`jawaban : ${jawaban1} tidak ada`);
+    
+  }
 }
+}
+var jawaban2 = prompt('apakah kak fahmi baik? A : Iya B : Salah C : Gatau')
+
+
+switch (true) {
+  case jawaban2 == 'A':
+    alert('Betul ka fahmi orang baik')
+    break;
+
+  default:
+    if (jawaban2 == 'B'){
+  alert('Salah, ka fahmi adalah orang baik');
+  
+
+  }
+
+
+   else {
+    if (jawaban2 == 'C'){
+      alert('harus tau kalo ka fahmi adalah orang baik');
+    
+      } else{
+        alert(`jawaban : ${jawaban2} tidak ada`);
+    
+  }
+}
+}
+var jawaban3 = prompt('apakah kak fahmi baik? A : Iya B : Salah C : Gatau')
+
+
+switch (true) {
+  case jawaban3 == 'A':
+    alert('Betul ka fahmi orang baik')
+    break;
+
+  default:
+    if (jawaban3 == 'B'){
+  alert('Salah, ka fahmi adalah orang baik');
+  
+
+  }
+
+
+   else {
+    if (jawaban3 == 'C'){
+      alert('harus tau kalo ka fahmi adalah orang baik');
+    
+      } else{
+        alert(`jawaban : ${jawaban3} tidak ada`);
+    
+  }
+}
+}
+
+
